d51bb27bf23d621f8359269ab12a05be9de4c850
Joshua Shew <joshua.t.shew@gmail.com>
2022-10-26 13:37:04 -0400

    added multi-line input

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
-import numpy as np
-
 def main():
-    paragraph = "　" + input("Paragraph: ")
+    # Get and process input
+    essay = getInput("Copy-paste the essay here:\n\n")[:-4]
+    paragraphs = splitEssay(essay)
+        
+    # Split lines
+    paragraphLines = []
+    for p in paragraphs:
+        paragraphLines.append(formattedParagraph(getLines(p)))
     
-    lines = getLines(paragraph)
-    text = formattedParagraph(lines)
-    print(text)
     
+    # Print output
+    print("\n" * 3)
+    for p in paragraphLines:
+        print(p + "\n")
+
+# Get multi-line input
+def getInput(prompt1, prompt2 = ""):
+    L = list()
+    prompt = prompt1
+    while True:
+        L.append(input(prompt))
+        prompt = prompt2
+        if inputComplete(L):
+            break
+    string = ""
+    for line in L:
+        string += line + "\n"
+    return string
+# Helper func for multi-line input
+def inputComplete(L):
+    if L[-1].count("d") > 0:
+        return True
+    else:
+        return False
+    
+
 # Ask user for folder path (allows for dragging)
 def getDraggablePath(message):
 	filePath = input(message + "\nDrag file/folder or paste path: ")
@@ -21,11 +49,23 @@ def isEndingPunctuation(character):
         if character == punctuation:
             return True
     return False
-    
+
+# Split an essay into paragraphs
+def splitEssay(essay):
+    paragraphs = essay.split("\n")
+    for p in paragraphs:
+        p = p.replace("　","")
+        p = p.replace("　","")
+        if len(p) == 0:
+            paragraphs.remove(p)
+    return paragraphs
+   
 # Divides a paragraph into lines
+# The input paragraph should have no spaces
 def getLines(paragraph, lineLength = 20):
-    if len(paragraph) == 0:
-        return np.array([])
+    paragraph = "　" + paragraph
+    if len(paragraph) == 1:
+        return []
     else:
         line = ""
         endIndex = lineLength
@@ -34,19 +74,21 @@ def getLines(paragraph, lineLength = 20):
         else:
             endIndex = lineLength + 1 if isEndingPunctuation(paragraph[lineLength]) else lineLength
         line = paragraph[:endIndex]
-        array = np.array([])
-        array = np.append(array, line)
+        array = []
+        array.append(line)
         newParagraph = paragraph[endIndex:]
-        return np.concatenate((array, getLines(newParagraph)))
-    
+        return array + getLines(newParagraph)
+
 # Formats a paragraph divided into lines for viewing
 def formattedParagraph(lines):
-    text = "" # start with a (Japanese) space
+    text = ""
     
-    for i in range(len(lines) - 1):
-        text += f"{lines[i]}\n"
-    if len(lines) != 0:
-        text += f"{lines[len(lines) - 1]}"
+    # Combine lines
+    for line in lines:
+        text += line + "\n"
+    
+    # Remove the last "\n"
+    text = text[:-1]
     
     return text
 
